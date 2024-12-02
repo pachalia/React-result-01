a35d3de03ab21d99dc26757d81d9153ebb360732
Andrey <pachalia2009@yandex.com>
2024-12-02 17:23:11 +0300

    App without JSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-// Здесь используется декларативный подход. Поскольку HTML Это хороший пример декларативного подхода.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-		  <p>{new Date().getFullYear()}</p>
-      </header>
-    </div>
-  );
+function App () {
+	return (
+		createElement(
+			'div', {className: 'App'},
+		createElement('header', {className: 'App-header'},
+			createElement('img', {src: logo, className: 'App-logo', alt: 'logo'}),
+			createElement(
+				'p', null, 'Edit ',
+				createElement('code', null, 'src/App.js'), ' and save to reload.'),
+			createElement('a',
+				{className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferer'},
+				'Learn React'),
+			createElement('p', null, new Date().getFullYear())
+			),
+
+		)
+	)
 }
 
 export default App;
